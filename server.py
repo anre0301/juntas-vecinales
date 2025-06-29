cf13141adab6f5047027010d39c8cf78164a8022
anre0301 <juliomedina030122@gmail.com>
2025-06-29 17:41:54 -0500

    modifique varios

--- server.py
+++ server.py
@@ -14,24 +14,78 @@ if os.path.exists(IMAGENES_JSON_PATH):
     with open(IMAGENES_JSON_PATH, "r", encoding="utf-8") as f:
         IMAGENES_DNI = json.load(f)
 else:
-    IMAGENES_DNI = {}
+    IMAGENES_DNI = {
+    "00811758": "https://i.ibb.co/NdRxHy2B/00811758.jpg",
+    "16429103": "https://i.ibb.co/pFvrLKb/16429103.jpg",
+    "16445545": "https://i.ibb.co/VYvrvdk1/16445545.jpg",
+    "16469856": "https://i.ibb.co/B23jc6wV/16469856.jpg",
+    "16478430": "https://i.ibb.co/LXxLMbQJ/16478430.jpg",
+    "16553559": "https://i.ibb.co/mVQY72xj/16553559.jpg",
+    "16598349": "https://i.ibb.co/rRh0M1HG/16598349.jpg",
+    "16628701": "https://i.ibb.co/d0JcXy4N/16628701.jpg",
+    "16678733": "https://i.ibb.co/svtb2VD9/16678733.jpg",
+    "16753914": "https://i.ibb.co/QFcRGxzx/16753914.jpg",
+    "16759681": "https://i.ibb.co/27j7M793/16759681.jpg",
+    "16782540": "https://i.ibb.co/m5Cnc8tM/16782540.jpg",
+    "16797673": "https://i.ibb.co/0jZPNC09/16797673.jpg",
+    "16801728": "https://i.ibb.co/WWNQq4fn/16801728.jpg",
+    "17637201": "https://i.ibb.co/C3LgW3T8/17637201.jpg",
+    "27263560": "https://i.ibb.co/Tx58JdwY/27263560.jpg",
+    "27269108": "https://i.ibb.co/rG07B0by/27269108.jpg",
+    "27397572": "https://i.ibb.co/3DrjkyJ/27397572.jpg",
+    "27413074": "https://i.ibb.co/k6yCYr6g/27413074.jpg",
+    "33663530": "https://i.ibb.co/JFQ98xb3/33663530.jpg",
+    "41101613": "https://i.ibb.co/rK48tLr7/41101613.jpg",
+    "41614718": "https://i.ibb.co/NgcW7bdB/41614718.jpg",
+    "41657863": "https://i.ibb.co/5g160PQ6/41657863.jpg",
+    "41669360": "https://i.ibb.co/tTxHc0RS/41669360.jpg",
+    "41794891": "https://i.ibb.co/GvsczSL4/41794891.jpg",
+    "42047423": "https://i.ibb.co/67fTT9wv/42047423.jpg",
+    "42189841": "https://i.ibb.co/dstSRH18/42189841.jpg",
+    "42290226": "https://i.ibb.co/TBXD2vHj/42290226.jpg",
+    "42753439": "https://i.ibb.co/Rk1sftQc/42753439.jpg",
+    "42866711": "https://i.ibb.co/nMMhzcQp/42866711.jpg",
+    "43229756": "https://i.ibb.co/G4LhRz01/43229756.jpg",
+    "43232240": "https://i.ibb.co/fYYZCR92/43232240.jpg",
+    "43856165": "https://i.ibb.co/yLxKYdW/43856165.jpg",
+    "44623614": "https://i.ibb.co/KxGW25cR/44623614.jpg",
+    "44764529": "https://i.ibb.co/VpzgHrn3/44764529.jpg",
+    "44912153": "https://i.ibb.co/1fg8yhCK/44912153.jpg",
+    "45067658": "https://i.ibb.co/ymdRHwHj/45067658.jpg",
+    "45155615": "https://i.ibb.co/7NgkRYhm/45155615.jpg",
+    "45281070": "https://i.ibb.co/YFXcbLKy/45281070.jpg",
+    "45513366": "https://i.ibb.co/VYdGRwN1/45513366.jpg",
+    "46079713": "https://i.ibb.co/spDh0W4n/46079713.jpg",
+    "46557468": "https://i.ibb.co/C3NzZtPt/46557468.jpg",
+    "46708207": "https://i.ibb.co/ccCsBx4W/46708207.jpg",
+    "47247874": "https://i.ibb.co/1SmdKM4/47247874.jpg",
+    "47764489": "https://i.ibb.co/HTB7nmgb/47764489.jpg",
+    "47848267": "https://i.ibb.co/vCx4dVzg/47848267.jpg",
+    "48440515": "https://i.ibb.co/r2sW11Gc/48440515.jpg",
+    "61016423": "https://i.ibb.co/RGqWX5jy/61016423.jpg",
+    "70895774": "https://i.ibb.co/VYstxG4D/70895774.jpg",
+    "71965621": "https://i.ibb.co/d4vjv0v8/71965621.jpg",
+    "74204169": "https://i.ibb.co/rKWKCTBJ/74204169.jpg",
+    "74739821": "https://i.ibb.co/C3XJX8JZ/74739821.jpg",
+    "75666315": "https://i.ibb.co/xqhBYxD1/75666315.jpg",
+    "76522510": "https://i.ibb.co/Hf8ZGCVs/76522510.jpg",
+    "80219071": "https://i.ibb.co/whD8XQYP/80219071.jpg",
+    "80219749": "https://i.ibb.co/8nDTCfnN/80219749.jpg",
+    "80256691": "https://i.ibb.co/HpTB7RhY/80256691.jpg",
+    "80642437": "https://i.ibb.co/20BNVz89/80642437.jpg"
+    }
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         dni = request.form.get("dni")
-        fecha = request.form.get("fecha")
         encontrado = None
-
-        if not re.fullmatch(r"\d{2}/\d{2}/\d{4}", fecha):
-            return render_template("index.html", error="❌ Ingrese la fecha con el formato correcto: DD/MM/AAAA")
 
         if os.path.exists(DATA_JSON_PATH):
             with open(DATA_JSON_PATH, "r", encoding="utf-8") as f:
                 data = json.load(f)
                 for persona in data:
-                    fecha_persona = persona.get("fecha_creacion") or persona.get("fecha")
-                    if persona.get("dni") == dni and fecha_persona == fecha:
+                    if persona.get("dni") == dni:
                         encontrado = persona
                         break
 
